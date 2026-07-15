0b80146fc5c70e5f7c859ace200128a4cb7ba1cf
Flexabler <smolnikov2023@list.ru>
2026-07-15 22:29:43 +0500

    add index template and render it.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__)
+    # Пришлось указать шаблон явно, так как Flask(__name__) определяет приложение и ищет его относительно текущего модуля
+    # Указывая явно, начинает смотреть корневую директорию проекта
+    template_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates')
+    app = Flask(__name__, template_folder=template_dir)
 
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
 
     @app.route('/')
     def index():
-        return 'Page Analyzer is running'
+        return render_template('index.html')
 
     return app
 
