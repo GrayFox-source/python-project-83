89eabe8833de73db2946659cd044f7ba51b59c00
Flexabler <smolnikov2023@list.ru>
2026-07-15 16:41:00 +0500

    First Step Commit. Add an app package, lint, Makefile

---
+++ page_analyzer/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
+
+    @app.route('/')
+    def index():
+        return 'Page Analyzer is running'
+
+    return app
+
+app = create_app()
+
